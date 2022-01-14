ab7aecb7d98b0876e65d5f69871dcab5f1e224cd
Ivan López <ivan.lopez1@unmsm.edu.pe>
2022-01-13 21:40:34 -0500

    dividir modulo en el home y las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,44 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
-import { HomeComponent } from './home/home.component';
+
+import { LayoutComponent } from './layout/layout.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductsComponent } from './products/products.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    pathMatch:'full'
+    component: LayoutComponent,
+
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch:'full',
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(m=>m.HomeModule)
+      },
+      {
+        path: 'products',
+        component: ProductsComponent
+      },
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      },
+    ]
   },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'products',
-    component: ProductsComponent
-  },
-  {
-    path: 'contact',
-    component: ContactComponent
-  },
+  
   {
     path: 'demo',
     component: DemoComponent
@@ -36,7 +50,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
